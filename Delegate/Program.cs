7ee421d8b765e89ad38986b0f233f295f8bf2ca8
khoih318 <khoih318@gmail.com>
2023-03-07 20:44:17 +0700

    Add project files.

---
+++ Delegate/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CS008_Anonymous
+{
+    //lehoangkhoi-20mc-20it438
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // Logs.TestShowLog();
+            // Logs.TestShowLogMulti();
+            // Logs.TestShowLogPlus();
+
+            // FuncAction.TestFunc(5, 6); // In ra: 11
+
+            FuncAction.TestTinhTong();
+        }
+
+    }
+}
